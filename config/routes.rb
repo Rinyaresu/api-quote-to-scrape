695560c436a8a0f2df4e8d2ab22efe5bd3ed984f
Your name <kaiquelinhares17@outlook.com>
2022-03-04 13:30:59 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :quotes, only: [:index]
+      get 'quotes/:search_tag', to: 'search#index'
+    end
+  end
 end
